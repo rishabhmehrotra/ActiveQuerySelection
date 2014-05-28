06208b1b13bd418b5078819a4549397a40ec2f7e
Rishabh Mehrotra <erishabh@gmail.com>
2014-05-28 08:04:09 +0100

    Minor modifications to ErrorBar code

--- src/CalculateErrorBars.java
+++ src/CalculateErrorBars.java
@@ -4,12 +4,19 @@ import java.util.*;
 public class CalculateErrorBars {
 
 	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new FileReader("src/data/LETOR/forEval/results/NDCG_errorBars_minAvgPL_50x10_correctPLFormula.txt"));
+		//BufferedReader br = new BufferedReader(new FileReader("src/data/LETOR/forEval/results/NDCG_errorBars_minAvgPL_50x10_correctPLFormula.txt"));
+		BufferedReader br = new BufferedReader(new FileReader("src/data/LETOR/forEval/results/NDCG_errorBars_onlyNDisagreement_50x10_all committees correct.txt"));
+		//BufferedReader br = new BufferedReader(new FileReader("src/data/LETOR/forEval/results/NDCG_errorBars_half PL half disagreement_50x10.txt"));
+		//BufferedReader br = new BufferedReader(new FileReader("src/data/LETOR/forEval/results/NDCG_RANDOM_errorBars_minAvgPL_50x10_correctPLFormula.txt"));
+		//BufferedReader br = new BufferedReader(new FileReader("src/data/LETOR/forEval/results/NDCG_errorBars_onlyLDASim_50x10.txt"));
 		String line = br.readLine();
+		int prev = 0;
 		while(line!= null)
 		{
 			String[] parts = line.split("\t");
 			int numQ = Integer.parseInt(parts[0]);
+			if(numQ == prev) {line = br.readLine();continue;}
+			else prev = numQ;
 			double avg=0;int c=0;
 			for(int i=1;i<parts.length;i++)
 			{
@@ -28,10 +35,11 @@ public class CalculateErrorBars {
 				stdDev += (diff*diff);
 			}
 			stdDev = Math.sqrt(stdDev);
-			stdDev /= c;
+			double error  = stdDev/c;
+			//stdDev /= c;
 			//System.out.print(stdDev+" ");
 			//System.out.print(avg+" ");
-			System.out.print(parts[0]+" ");
+			System.out.println((Integer.parseInt(parts[0]))+" "+avg+" "+error);
 			line= br.readLine();
 		}
 		
